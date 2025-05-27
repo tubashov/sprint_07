3efd37260d092105a950e61ef15aa6ac15016c9a
tubashov <tubashov@yandex.ru>
2025-05-27 17:40:01 +0300

    poem_fixer

---
+++ poem-fixer/src/PoemFixer.java
@@ -0,0 +1,32 @@
+public class PoemFixer {
+
+    public String[] readPoem() {
+        return new String[]{
+                "   Это кто там ложку «ложит»?",
+                "",
+                "   ",
+                "Знай, такого быть не может!",
+                "  Ложку мы на стол кладём,  ",
+                "",
+                "А тебя – к обеду ждём."
+        };
+    }
+
+    public String fixPoem(String[] poem) {
+        // допишите код
+        StringBuilder sb = new StringBuilder();
+        for (String line: poem) {
+            if (!line.isEmpty() && !line.isBlank()) {
+                sb.append(line.trim()).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        PoemFixer poemFixer = new PoemFixer();
+        String[] poem = poemFixer.readPoem();
+        String poemAsString = poemFixer.fixPoem(poem);
+        System.out.println(poemAsString);
+    }
+}
